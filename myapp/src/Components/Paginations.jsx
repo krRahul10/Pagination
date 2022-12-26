9bae39b39994a14a212aeff6d5aa72b846527cf6
Rahul <rahul10kr7@gmail.com>
2022-12-26 20:33:38 +0530

    done

--- myapp/src/Components/Paginations.jsx
+++ myapp/src/Components/Paginations.jsx
@@ -2,25 +2,38 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import "./pagination.css";
-// import '../App.css';
 
 const Paginations = () => {
-  const [products, setProduct] = useState([]);
+  //'https://dummyjson.com/products?limit=10&skip=10&select=title,price'
+  // slice(page * 10 - 10, page * 10)
 
-//   console.log("prod", products);
+  const [products, setProduct] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
   const fetchData = async () => {
-    const res = await fetch("https://dummyjson.com/products");
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+    );
     const data = await res.json();
 
     if (data && data.products) {
       setProduct(data.products);
+      setTotalPages(data.total / 10);
     }
-    console.log("data pagination", data);
   };
 
+  const setPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= totalPages &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page]);
   return (
     <div>
       <h1>Pagination</h1>
@@ -29,12 +42,40 @@ const Paginations = () => {
         <div className="products">
           {products.map((el, index) => {
             return (
-              <span className="products__single" key={el.id}> 
+              <span className="products__single" key={el.id}>
                 <img src={el.thumbnail} alt={el.title} />
                 <span>{el.title}</span>
               </span>
             );
           })}
+        </div>
+      )}
+
+      {products.length && (
+        <div className="pagination">
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick={() => setPageHandler(page - 1)}
+          >
+            ◀️
+          </span>
+          {[...Array(totalPages)].map((_, index) => {
+            return (
+              <span
+                className={page === index + 1 ? "pagination__selected" : ""}
+                key={index}
+                onClick={() => setPageHandler(index + 1)}
+              >
+                {index + 1}
+              </span>
+            );
+          })}
+          <span
+            className={page < totalPages ? "" : "pagination__disable"}
+            onClick={() => setPageHandler(page + 1)}
+          >
+            ▶️
+          </span>
         </div>
       )}
     </div>
